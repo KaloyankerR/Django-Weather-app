d15380fa5cb7ebb7d954555c9c22ebe2f0beabb4
KaloyankerR <kaloyankulov2003kk@gmail.com>
2023-01-21 14:36:20 +0100

    Adding cities functionality finished

--- weather/base/views.py
+++ weather/base/views.py
@@ -1,10 +1,18 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
+
 
 # Create your views here.
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=b15f9fd26448fd72a810d6c7bfe7bb74&units=metric'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save() 
+
+    form = CityForm()
     cities = City.objects.all()
     cities_data = []
 
@@ -19,5 +27,5 @@ def index(request):
         }
         cities_data.append(weather_data)
 
-    context = {'cities_data': cities_data}
+    context = {'cities_data': cities_data, 'form': form}
     return render(request, 'base/index.html', context)
